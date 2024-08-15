7dc63d6e07655d4282410e1ec4047f693572cf11
Arisekola <shuaibazeez14@gmail.com>
2024-08-15 20:11:40 +0100

    error fixed

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,26 +1,28 @@
-
 import { NextResponse } from 'next/server';
 import { getUser } from '../../../../actions/getUser';
 import prisma from '@/lib/prisma'
 
 export async function POST(req: Request) {
-
-    const currentUser = await getUser()
-
   try {
     const body = await req.json();
 
     const { title, content, category, imageUrl } = body;
 
+    
+    const currentUser = await getUser();
+
+    if (!currentUser || !currentUser.id || !currentUser.name) {
+      return NextResponse.json({ error: 'User not authenticated or missing user information' }, { status: 400 });
+    }
 
     const blog = await prisma.blog.create({
       data: {
         content,
         title,
         category,
-        images:imageUrl,
-        userId: currentUser?.id,
-        author: currentUser?.name,
+        image: imageUrl,
+        userId: currentUser.id,
+        author: currentUser.name,
       }
     });
 
@@ -30,4 +32,3 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Error creating blog' }, { status: 500 });
   }
 }
-

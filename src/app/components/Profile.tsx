aa00ec2aa1d593bbb4bc3e8f152186fb33a389fb
Arisekola <shuaibazeez14@gmail.com>
2024-08-15 23:46:13 +0100

    server fixed

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 import { FaBell, FaUserAlt } from "react-icons/fa";
 import { MdHome, MdSearch, MdMessage, MdPostAdd } from "react-icons/md";
--- src/app/profile/Profile.tsx
+++ src/app/profile/Profile.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image';
 import { formatDistanceToNow } from 'date-fns';
 import Post from '../recent-post/Post';

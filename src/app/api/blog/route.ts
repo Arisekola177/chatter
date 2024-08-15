b84483592ec72253b51702ec8f7b411dd1e87cfc
Arisekola <shuaibazeez14@gmail.com>
2024-08-15 20:12:27 +0100

    fixed

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
         content,
         title,
         category,
-        image: imageUrl,
+        images: imageUrl,
         userId: currentUser.id,
         author: currentUser.name,
       }

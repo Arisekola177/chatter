9b81d6cffbc090831579a87d039da395eddceadf
Arisekola <shuaibazeez14@gmail.com>
2024-08-16 08:29:34 +0100

    error fixed

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,27 +1,7 @@
-// import React from 'react'
-// import RegisterForm from './RegisterForm'
-
-// const page = () => {
-//   return (
-//     <div className="w-8/12 h-screen mx-auto flex justify-center items-center">
-//         <RegisterForm />
-//     </div>
-//   )
-// }
-
-// export default page
-
 import React from 'react'
 import RegisterForm from './RegisterForm'
-
-
-export const getServerSideProps = async () => {
-  return {
-    props: {}, 
-  }
-}
-
-const Page = () => {
+export const dynamic = 'force-dynamic';
+const page = () => {
   return (
     <div className="w-8/12 h-screen mx-auto flex justify-center items-center">
         <RegisterForm />
@@ -29,4 +9,8 @@ const Page = () => {
   )
 }
 
-export default Page
+export default page
+
+
+
+

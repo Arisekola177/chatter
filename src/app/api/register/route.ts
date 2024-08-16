a1fc7dfcc32a24026fcf869c663fe5fbdc638330
Arisekola <shuaibazeez14@gmail.com>
2024-08-16 08:15:47 +0100

    register error fixed

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,29 +1,34 @@
+
 import bcrypt from 'bcryptjs';
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
+export async function POST(req: Request): Promise<NextResponse> {
+  try {
+    const body = await req.json();
 
-export async function POST(req:Request) {
-  const body = await req.json();
+    const { email, password, name } = body;
 
-  const { email, password, name } = body;
+    // Hash the password
+    const hassPassword = await bcrypt.hash(password, 10);
 
-  const hassPassword = await bcrypt.hash(password, 10);
+    const defaultImageUrl = "https://randomuser.me/api/portraits/men/1.jpg";
 
-  const defaultImageUrl = "https://randomuser.me/api/portraits/men/1.jpg";
+    // Create a new user in the database
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        hassPassword, 
+        image: defaultImageUrl,
+      },
+    });
 
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      hassPassword,
-      image: defaultImageUrl,
-    },
-  });
-
-  return NextResponse.json(user);
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
+  }
 }
-
 
 
 
